c4894f1e7b45dbe3d6e81c4034e357f25934bab7
Holger Just <web@meine-er.de>
2011-01-29 21:05:00 +0100

    Skip unnecessary before filters.
    Always answer to checks regardless of authentication settings. Also don't do
    any superfluous SQL queries to get settings which are not needed.

--- app/controllers/check_controller.rb
+++ app/controllers/check_controller.rb
@@ -1,4 +1,9 @@
 class CheckController < ApplicationController
+  
+  # Skip the default filters from ApplicatipnController.
+  # They are not needed here as we don't do anything fancy.
+  skip_before_filter :user_setup, :check_if_login_required, :set_localization
+  
   def index
     ActiveRecord::Base.connection.execute("SELECT 1;")
     render :text => "ALIVE", :status => 200
