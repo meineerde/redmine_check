97596b81b4a0c44dfa4c38d5bd3f9f0cc4ca50ff
Holger Just <web@meine-er.de>
2011-03-01 15:31:17 +0100

    Better comments, fix typos

--- app/controllers/check_controller.rb
+++ app/controllers/check_controller.rb
@@ -1,9 +1,10 @@
 class CheckController < ApplicationController
-  
-  # Skip the default filters from ApplicatipnController.
+  # Skip the default filters from ApplicationController.
   # They are not needed here as we don't do anything fancy.
   skip_before_filter :user_setup, :check_if_login_required, :set_localization
-  
+
+  # perform a check select on the database and return with HTTP 200 if it
+  # succeeded or HTTP 500 if it failed for some reason
   def index
     ActiveRecord::Base.connection.execute("SELECT 1;")
     render :text => "ALIVE", :status => 200
@@ -11,6 +12,7 @@ class CheckController < ApplicationController
     render :text => "ERROR",  :status => 500
   end
 
+  # Do not log the checks into the default log.
   def logger
     @logger ||= Logger.new(StringIO.new)
   end    
