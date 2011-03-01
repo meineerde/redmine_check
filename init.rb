e38fe5de60e5dcefdbdc2abc892b0d7062541e62
Holger Just <web@meine-er.de>
2011-03-01 15:34:57 +0100

    typo

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_check do
   name 'Redmine Check plugin'
-  url 'http://dev.holgerjust.de/projects/redmine-misc'
+  url 'http://dev.holgerjust.de/projects/redmine-check'
   author 'Holger Just'
   author_url 'http://meine-er.de'
   description 'Add a simple and very cheap action to check whether the application stack is still alive.'
