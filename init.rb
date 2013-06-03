b336a4008a85c24c786c34190ac919f180695106
Holger Just <hjust@meine-er.de>
2013-06-03 10:48:56 +0200

    Check if we run inside of a Redmine(-like) environment

--- init.rb
+++ init.rb
@@ -1,10 +1,14 @@
-require 'redmine'
+begin
+  require 'redmine'
 
-Redmine::Plugin.register :redmine_check do
-  name 'Redmine Check plugin'
-  url 'http://dev.holgerjust.de/projects/redmine-check'
-  author 'Holger Just'
-  author_url 'http://meine-er.de'
-  description 'Add a simple and very cheap action to check whether the application stack is still alive.'
-  version '0.1'
+  Redmine::Plugin.register :redmine_check do
+    name 'Redmine Check plugin'
+    url 'http://dev.holgerjust.de/projects/redmine-check'
+    author 'Holger Just'
+    author_url 'http://meine-er.de'
+    description 'Add a simple and very cheap action to check whether the application stack is still alive.'
+    version '0.1'
+  end
+rescue LoadError
+  # Don't register the plugin, if we don't run in a Redmine(-like) environment
 end
